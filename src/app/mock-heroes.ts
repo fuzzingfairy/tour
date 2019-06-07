a6e4858d179bd6f7ddc1dc4769b5ca30aea75805
alicelambda <alicereuterdev@gmail.com>
2019-06-07 07:48:40 -0500

    split hero component and began adding service

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {id: 11, name: "dr nice"},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'}
+]
